aec587c009409cd8bfca639a5817c9b1cd3f2486
jacobcwright <jacob.wright.ut@gmail.com>
2019-02-15 23:37:26 -0700

    Final Product

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ const Enter = function(){
     console.log("Clicked");
 }
 
-const url = "https://wger.de/api/v2/exerciseiinfo/";
-fetch(url)
-    .then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        let results = "";
+document.getElementById("animeSubmit").addEventListener("click", function (event) {
+    event.preventDefault();
+    const value = document.getElementById("animeInput").value;
+    if (value === "")
+        return;
+    console.log(value);
 
-    })
+    const url = "https://api.jikan.moe/v3/search/anime?q=" + value + "&page=1";
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        }).then(function (json) {
+                if(json.response === "'error':'Server Error'"){
+                    alert("anime not found");
+                }
+        //var parsedData = JSON.parse(json);
+        let results = "";
+            results += "<h1>" + json.results[0].title + "</h1>"; 
+            results += "<h1> Rating: " + json.results[0].score + "/10</h1>";
+            results += "<img src=\"" + json.results[0].image_url + "\"/>";
+            results += "<h1>" + json.results[0].rated + "</h1>";
+            results += "<h2>Synopsis: </h1>";
+            results += "<p>" + json.results[0].synopsis + "<p>";
+            results += "<p><b>Episodes:</b> " + json.results[0].episodes + "</p>";
+            //console.log(json.results[0].rated);
+            document.getElementById("anime-results").innerHTML = results;
+        });
+});
